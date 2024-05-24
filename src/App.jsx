1e44fc7a015e1dff2956481ab694c1ea827220d0
dungmv <maivandung91@gmail.com>
2024-05-25 02:57:46 +0700

    update ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,90 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import './App.css';
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+  const [width, setWidth] = useState(150);
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
 
-  return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+  const [sidebarWidth, setSidebarWidth] = useState(200);
+  const [isSidebarResizing, setIsSidebarResizing] = useState(false);
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  const handleMouseDown = () => setIsSidebarResizing(true);
+
+  const handleMouseMove = (e) => {
+    if (isSidebarResizing) {
+      setSidebarWidth(e.clientX);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsSidebarResizing(false);
+  };
+
+  return (<div className="grid w-full h-full"
+    onMouseMove={handleMouseMove}
+    onMouseUp={handleMouseUp}
+    style={{
+      gridTemplateRows: 'auto minmax(0px, 1fr)',
+      gridTemplateColumns: `${sidebarWidth}px 0px 1fr`,
+      gridTemplateAreas: `"head head head" "side drag body"`
+    }}
+  >
+    <div className="overflow-hidden bg-gray-100" style={{
+      gridRowStart: 'side',
+      gridColumnStart: 'side',
+      gridRowEnd: 'side',
+      gridColumnEnd: 'side'
+    }}>Sidebar</div>
+    <div className="-translate-x-3 group z-10 flex h-full w-3 cursor-col-resize justify-end right-0"
+      onMouseDown={handleMouseDown}
+      style={{ gridArea: 'drag' }} />
+    <div className="h-md pt-[1px] w-full border-b min-w-0 pl-20 pr-1" style={{
+      gridRowStart: 'head',
+      gridColumnStart: 'head',
+      gridRowEnd: 'head',
+      gridColumnEnd: 'head'
+    }}>Header</div>
+    <div className="p-3 gap-1.5 grid w-full h-full" style={{
+      gridRowStart: 'body',
+      gridColumnStart: 'body',
+      gridRowEnd: 'body',
+      gridColumnEnd: 'body',
+      gridTemplateRows: 'minmax(0px, 1fr)',
+      gridTemplateColumns: `${sidebarWidth}px 0px 1fr`,
+      gridTemplateAreas: `"left drag right"`
+    }}>
+      <div className="h-full grid grid-rows-[auto_minmax(0,1fr)] grid-cols-1 bg-slate-300" style={{
+        gridRowStart: 'left',
+        gridColumnStart: 'left',
+        gridRowEnd: 'left',
+        gridColumnEnd: 'left'
+      }}>
+        Request
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
+      <div className="-translate-x-1.5 group z-10 flex h-full w-3 cursor-col-resize justify-center left-0" style={{
+        gridRowStart: 'drag',
+        gridColumnStart: 'drag',
+        gridRowEnd: 'drag',
+        gridColumnEnd: 'drag'
+      }} />
+      <div className="h-full grid grid-rows-[auto_minmax(0,1fr)] grid-cols-1 bg-blue-200" style={{
+        gridRowStart: 'right',
+        gridColumnStart: 'right',
+        gridRowEnd: 'right',
+        gridColumnEnd: 'right'
+      }}>
+        Response
+      </div>
     </div>
-  );
+  </div>);
 }
 
 export default App;
