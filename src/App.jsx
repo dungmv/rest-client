088e16877134dbebab8b8839fa3adc70f8fd2ac7
dungmv <maivandung91@gmail.com>
2024-05-27 15:31:50 +0700

    title bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React, { useEffect, useRef, useState } from "react";
+import { appWindow } from "@tauri-apps/api/window";
+import { window as tauriWindow } from "@tauri-apps/api";
 import { invoke } from "@tauri-apps/api/tauri";
 import './App.css';
 
@@ -13,22 +14,42 @@ function App() {
     setGreetMsg(await invoke("greet", { name }));
   }
 
+  const [title, setTitle] = useState("");
+  const win = tauriWindow.getCurrent();
+
+  useEffect(() => {
+    win.title().then(setTitle);
+  }, [win]);
+
+  const [isScaleup, setScaleup] = useState(false);
+  // .minimize() - to minimize the window
+  const onMinimize = () => appWindow.minimize();
+  const onScaleup = () => {
+    // .toggleMaximize() - to swap the window between maximize and minimum
+    appWindow.toggleMaximize();
+    setScaleup(true);
+  }
+
+  const onScaledown = () => {
+    appWindow.toggleMaximize();
+    setScaleup(false);
+  }
+
+  // .close() - to close the window
+  const onClose = () => appWindow.close();
+
   const [requestWidth, setRequestWidth] = useState(0.5);
-
   const [sidebarWidth, setSidebarWidth] = useState(200);
-
 
   const resizeRequest = (e) => {
     const { width, left } = contentRef.current.getBoundingClientRect()
     const fr = (e.clientX - left) / width;
     setRequestWidth(fr);
-    console.log(fr)
   }
 
   const resizeSidebar = (e) => {
     setSidebarWidth(e.clientX);
   };
-
 
   return (<div className="grid w-full h-full"
     style={{
@@ -41,7 +62,9 @@ function App() {
     <div className="-translate-x-3 group z-10 flex h-full w-3 cursor-col-resize justify-end right-0"
       draggable={true} onDrag={resizeSidebar}
       style={{ gridArea: 'drag' }} />
-    <div className="h-md pt-[1px] w-full border-b min-w-0 pl-20 pr-1" style={{ gridArea: 'head' }}>Header</div>
+    <div className="h-md pt-[1px] h-8 w-full border-b min-w-0 pl-20 pr-1" style={{ gridArea: 'head' }} data-tauri-drag-region>
+      <span>{title}</span>
+    </div>
     <div ref={contentRef} className="p-3 gap-1.5 grid w-full h-full" style={{
       gridArea: 'body',
       gridTemplateRows: 'minmax(0px, 1fr)',
@@ -49,7 +72,7 @@ function App() {
       gridTemplateAreas: `"left drag right"`
     }}>
       <div className="h-full grid grid-rows-[auto_minmax(0,1fr)] grid-cols-1 bg-slate-300" style={{ gridArea: 'left' }}>
-        Request
+        <input type="text" className="form-input rounded-md" value={name} onChange={(e) => setName(e.target.value)} />
       </div>
       <div draggable={true} className="-translate-x-1.5 group z-10 flex h-full w-3 cursor-col-resize justify-center left-0"
         onDrag={resizeRequest}
